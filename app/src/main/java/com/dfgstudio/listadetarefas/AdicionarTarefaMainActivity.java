a12bb48e3b683071b7bd64f4a4623048e6f2a8d2
gentildf <gentildf@gmail.com>
2021-12-30 21:54:46 -0300

    Comit inicial do app

---
+++ app/src/main/java/com/dfgstudio/listadetarefas/AdicionarTarefaMainActivity.java
@@ -0,0 +1,14 @@
+package com.dfgstudio.listadetarefas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionarTarefaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefa_main);
+    }
+}
