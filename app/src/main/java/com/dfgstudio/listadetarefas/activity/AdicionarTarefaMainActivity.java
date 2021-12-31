1bfe4d1aa8224876906437f7ce06dd6b754cc34f
gentildf <gentildf@gmail.com>
2021-12-31 11:47:24 -0300

    Menu previamente configurado e criado evento de clique.

--- app/src/main/java/com/dfgstudio/listadetarefas/activity/AdicionarTarefaMainActivity.java
+++ app/src/main/java/com/dfgstudio/listadetarefas/activity/AdicionarTarefaMainActivity.java
@@ -1,8 +1,11 @@
 package com.dfgstudio.listadetarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.dfgstudio.listadetarefas.R;
 
@@ -12,5 +15,24 @@ public class AdicionarTarefaMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch ( item.getItemId() ){
+            case R.id.itemSalvar:
+
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
