3c1a55e4c4c3758d3e7e3eee61e05bbd4181c344
gentildf <gentildf@gmail.com>
2022-01-03 19:51:09 -0300

    corrigido icone da pagina inicial

--- app/src/main/java/com/dfgstudio/listadetarefas/activity/MainActivity.java
+++ app/src/main/java/com/dfgstudio/listadetarefas/activity/MainActivity.java
@@ -166,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
         carregarListaTarefas();
         super.onStart();
     }
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -189,4 +189,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+ */
 }
